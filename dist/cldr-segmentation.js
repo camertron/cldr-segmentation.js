827f447bd3d3b7d6259e3f546e337c7e0ba3c069
Kevin Soulat <kevin.soulat@outlook.com>
2020-03-06 16:56:30 -0500

    fix - wrong utfstring module name

--- dist/cldr-segmentation.js
+++ dist/cldr-segmentation.js
@@ -1,13 +1,13 @@
 (function (global, factory) {
   if (typeof define === "function" && define.amd) {
-    define('cldr/segmentation', ['exports', 'UtfString'], factory);
+    define('cldr/segmentation', ['exports', 'utfstring'], factory);
   } else if (typeof exports !== "undefined") {
-    factory(exports, require('UtfString'));
+    factory(exports, require('utfstring'));
   } else {
     var mod = {
       exports: {}
     };
-    factory(mod.exports, global.UtfString);
+    factory(mod.exports, global.utfstring);
     global.cldrSegmentation = mod.exports;
   }
 })(this, function (exports, utfstring) {
